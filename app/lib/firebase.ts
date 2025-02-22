cc997c5203f97161258d70a31e8f8dbf346b2953
grant-thomas <grant3192@aim.com>
2025-02-22 16:53:19 -0600

    Add keys to .env.local

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -3,14 +3,15 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyACNT0xthbd4eYSkZkw50KEXGtzU-P2VGs',
-	authDomain: 'ski-trip-recipes.firebaseapp.com',
-	projectId: 'ski-trip-recipes',
-	storageBucket: 'ski-trip-recipes.appspot.com',
-	messagingSenderId: '261138126328',
-	appId: '1:261138126328:web:20a3c9de08747761d0f20d',
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+// Ensure Firebase is initialized only once
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
